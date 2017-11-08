d7acb7019804040a86d25171dce62db05ede98a3
georgea18-meet <abudauodg@gmail.com>
2017-11-08 19:15:50 +0200

    COMMENT

---
+++ temre.py
@@ -0,0 +1,10 @@
+class Animal(object)
+    def __init__(self, sound, name, age, favorite_number)
+        self.sound=sound
+        self.name= name
+        self.age=age
+        self.favorite_number=favorite_number
+    def eat(self, food)
+        print("this food is not so good, but not so bad. it's:"+self.food)
+    def description(self)
+        print(self.name+"is the name of the"+self.age+" year old animal that likes"+ self.favorite)
